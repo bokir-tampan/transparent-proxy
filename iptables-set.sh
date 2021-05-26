111c89e854dbb16da4ee2ee4bcbe0371dce37e0c
ChenTanyi <chentanyi@gmail.com>
2021-05-26 16:17:33 +0800

    fix: add private network in mask to avoid loopback

--- iptables-set.sh
+++ iptables-set.sh
@@ -17,6 +17,10 @@ iptables -t mangle -A PREROUTING -j V2RAY
 iptables -t mangle -N V2RAY_MASK
 iptables -t mangle -A V2RAY_MASK -j RETURN -m mark --mark $SO_MARK
 iptables -t mangle -A V2RAY_MASK -p udp --dport 53 $MASK_DNS_OPTIONS
+iptables -t mangle -A V2RAY_MASK -d 127.0.0.1/32 -j RETURN
+iptables -t mangle -A V2RAY_MASK -d 10.0.0.0/8 -j RETURN
+iptables -t mangle -A V2RAY_MASK -d 172.16.0.0/12 -j RETURN
+iptables -t mangle -A V2RAY_MASK -d 192.168.0.0/16 -j RETURN
 iptables -t mangle -A V2RAY_MASK -p udp -j MARK --set-mark $TPROXY_MARK
 iptables -t mangle -A V2RAY_MASK -p tcp -j MARK --set-mark $TPROXY_MARK
 iptables -t mangle -A OUTPUT -j V2RAY_MASK
